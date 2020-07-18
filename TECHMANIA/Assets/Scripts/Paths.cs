f4cf498a59db7357869f7e6dede5e3b89192fa04
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-07-18 12:48:12 -0700

    Refreshing the song grid.

---
+++ TECHMANIA/Assets/Scripts/Paths.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class Paths
+{
+    public static string GetSongFolder()
+    {
+        string current = Directory.GetCurrentDirectory();  // Does not end with \
+        // If there's a "Builds" folder, assume we are running from
+        // Unity editor.
+        if (Directory.Exists(current + "\\Builds"))
+        {
+            current += "\\Builds";
+        }
+
+        string songs = current + "\\Songs";
+        if (!Directory.Exists(songs))
+        {
+            Directory.CreateDirectory(songs);
+        }
+        return songs;
+    }
+}
