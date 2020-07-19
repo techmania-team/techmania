c1cc2ede414d5b72c99fabfce74ca9e046d0db66
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-07-19 16:19:10 -0700

    Fixed bug where track list items become abnormally large if they are created before Start.

---
+++ TECHMANIA/Assets/Scripts/Components/ResetScale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
