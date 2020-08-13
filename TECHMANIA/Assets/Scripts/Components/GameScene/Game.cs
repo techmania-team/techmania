77b3bd3f5d052543843b660fc0f0fd0e49c63b68
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-08-12 23:22:51 -0700

    Refactored input handling so touch events are propagated the same way clicks are.

--- TECHMANIA/Assets/Scripts/Components/GameScene/Game.cs
+++ TECHMANIA/Assets/Scripts/Components/GameScene/Game.cs
@@ -5,12 +5,15 @@ using System.Diagnostics;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
 
 public class Game : MonoBehaviour
 {
     [Header("Scans")]
+    public GraphicRaycaster raycaster;
     public Transform topScanContainer;
     public GameObject topScanTemplate;
     public Transform bottomScanContainer;
@@ -42,6 +45,8 @@ public class Game : MonoBehaviour
     public static event UnityAction<int> ScanChanged;
     public static event UnityAction<int> ScanAboutToChange;
 
+    private static List<List<KeyCode>> keysForLane;
+
     private class NoteWithSound
     {
         public Note note;
@@ -57,7 +62,8 @@ public class Game : MonoBehaviour
         }
         resourceLoader.LoadResources(GameSetup.trackPath);
 
-        NoteObject.NoteResolved += OnNoteResolved;
+        // Input.simulateMouseWithTouches = false;
+        NoteObject.NoteBreak += OnNoteBreak;
 
         // And now we wait for the resources to load.
         stopwatch = null;
@@ -66,65 +72,11 @@ public class Game : MonoBehaviour
 
     private void OnDestroy()
     {
-        NoteObject.NoteResolved -= OnNoteResolved;
+        Input.simulateMouseWithTouches = true;
+        NoteObject.NoteBreak -= OnNoteBreak;
     }
 
-    // By default -3 / 2 = -1 because reasons. We want -2.
-    // This assumes b is positive.
-    private int RoundingDownIntDivision(int a, int b)
-    {
-        if (a % b == 0) return a / b;
-        if (a >= 0) return a / b;
-        return (a / b) - 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (stopwatch == null)
-        {
-            // Game not started yet.
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            OnPauseButtonClickOrTouch();
-        }
-
-        UpdateTime();
-    }
-
-    private void UpdateTime()
-    {
-        float newTime = (float)stopwatch.Elapsed.TotalSeconds
-            + initialTime;
-        FloatPulse = GameSetup.pattern.TimeToPulse(newTime);
-        int newPulse = Mathf.FloorToInt(FloatPulse);
-        int newScan = Mathf.FloorToInt(FloatPulse / PulsesPerScan);
-
-        if (Time < 0f && newTime >= 0f)
-        {
-            backingTrackSource.timeSamples = Mathf.FloorToInt(
-                newTime * backingTrackSource.clip.frequency);
-            backingTrackSource.Play();
-        }
-        Time = newTime;
-        // Fire ScanAboutToChange if we are 7/8 into the next scan.
-        if (RoundingDownIntDivision(Pulse + PulsesPerScan / 8, PulsesPerScan) !=
-            RoundingDownIntDivision(newPulse + PulsesPerScan / 8, PulsesPerScan))
-        {
-            ScanAboutToChange?.Invoke(
-                (newPulse + PulsesPerScan / 8) / PulsesPerScan);
-        }
-        Pulse = newPulse;
-        if (newScan > Scan)
-        {
-            ScanChanged?.Invoke(newScan);
-        }
-        Scan = newScan;
-    }
-
+    #region Initialization
     private IEnumerator LoadAndStartGame()
     {
         yield return new WaitUntil(() =>
@@ -138,7 +90,7 @@ public class Game : MonoBehaviour
         if (GameSetup.pattern.patternMetadata.controlScheme == ControlScheme.Keys ||
             GameSetup.pattern.patternMetadata.controlScheme == ControlScheme.KM)
         {
-            NoteObject.InitializeKeysForLane();
+            InitializeKeysForLane();
         }
 
         // Calculations.
@@ -214,6 +166,247 @@ public class Game : MonoBehaviour
         Scan--;
     }
 
+    public static void InitializeKeysForLane()
+    {
+        keysForLane = new List<List<KeyCode>>();
+        keysForLane.Add(new List<KeyCode>()
+        {
+            KeyCode.BackQuote,
+            KeyCode.Alpha0,
+            KeyCode.Alpha1,
+            KeyCode.Alpha2,
+            KeyCode.Alpha3,
+            KeyCode.Alpha4,
+            KeyCode.Alpha5,
+            KeyCode.Alpha6,
+            KeyCode.Alpha7,
+            KeyCode.Alpha8,
+            KeyCode.Alpha9,
+            KeyCode.Minus,
+            KeyCode.Equals,
+            KeyCode.KeypadDivide,
+            KeyCode.KeypadMultiply,
+            KeyCode.KeypadMinus
+        });
+        keysForLane.Add(new List<KeyCode>()
+        {
+            KeyCode.Q,
+            KeyCode.W,
+            KeyCode.E,
+            KeyCode.R,
+            KeyCode.T,
+            KeyCode.Y,
+            KeyCode.U,
+            KeyCode.I,
+            KeyCode.O,
+            KeyCode.P,
+            KeyCode.LeftBracket,
+            KeyCode.RightBracket,
+            KeyCode.Backslash,
+            KeyCode.Keypad7,
+            KeyCode.Keypad8,
+            KeyCode.Keypad9
+        });
+        keysForLane.Add(new List<KeyCode>()
+        {
+            KeyCode.A,
+            KeyCode.S,
+            KeyCode.D,
+            KeyCode.F,
+            KeyCode.G,
+            KeyCode.H,
+            KeyCode.J,
+            KeyCode.K,
+            KeyCode.L,
+            KeyCode.Semicolon,
+            KeyCode.Quote,
+            KeyCode.Keypad4,
+            KeyCode.Keypad5,
+            KeyCode.Keypad6
+        });
+        keysForLane.Add(new List<KeyCode>()
+        {
+            KeyCode.Z,
+            KeyCode.X,
+            KeyCode.C,
+            KeyCode.V,
+            KeyCode.B,
+            KeyCode.N,
+            KeyCode.M,
+            KeyCode.Comma,
+            KeyCode.Period,
+            KeyCode.Slash,
+            KeyCode.Keypad1,
+            KeyCode.Keypad2,
+            KeyCode.Keypad3
+        });
+    }
+    #endregion
+
+    // By default -3 / 2 = -1 because reasons. We want -2.
+    // This assumes b is positive.
+    private int RoundingDownIntDivision(int a, int b)
+    {
+        if (a % b == 0) return a / b;
+        if (a >= 0) return a / b;
+        return (a / b) - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopwatch == null)
+        {
+            // Game not started yet.
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnPauseButtonClickOrTouch();
+        }
+
+        UpdateTime();
+
+        ControlScheme scheme = GameSetup.pattern.patternMetadata.controlScheme;
+        if (scheme == ControlScheme.KM && Input.GetMouseButtonDown(0))
+        {
+            Raycast(Input.mousePosition);
+        }
+        else if (scheme == ControlScheme.Touch)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch t = Input.GetTouch(i);
+                if (t.phase == TouchPhase.Began)
+                {
+                    Raycast(t.position);
+                }
+            }
+        }
+    }
+
+    private void UpdateTime()
+    {
+        float newTime = (float)stopwatch.Elapsed.TotalSeconds
+            + initialTime;
+        FloatPulse = GameSetup.pattern.TimeToPulse(newTime);
+        int newPulse = Mathf.FloorToInt(FloatPulse);
+        int newScan = Mathf.FloorToInt(FloatPulse / PulsesPerScan);
+
+        if (Time < 0f && newTime >= 0f)
+        {
+            backingTrackSource.timeSamples = Mathf.FloorToInt(
+                newTime * backingTrackSource.clip.frequency);
+            backingTrackSource.Play();
+        }
+        Time = newTime;
+        // Fire ScanAboutToChange if we are 7/8 into the next scan.
+        if (RoundingDownIntDivision(Pulse + PulsesPerScan / 8, PulsesPerScan) !=
+            RoundingDownIntDivision(newPulse + PulsesPerScan / 8, PulsesPerScan))
+        {
+            ScanAboutToChange?.Invoke(
+                (newPulse + PulsesPerScan / 8) / PulsesPerScan);
+        }
+        Pulse = newPulse;
+        if (newScan > Scan)
+        {
+            ScanChanged?.Invoke(newScan);
+        }
+        Scan = newScan;
+    }
+
+    private void Raycast(Vector2 screenPosition)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = screenPosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(eventData, results);
+
+        foreach (RaycastResult r in results)
+        {
+            NoteObject n = r.gameObject.GetComponent<NoteObject>();
+            if (n != null)
+            {
+                float correctTime = n.note.time;
+                float difference = Time - correctTime;
+                if (Mathf.Abs(difference) > kBreakThreshold)
+                {
+                    // The touch or click is too early or too late
+                    // for this note. Ignore.
+                    continue;
+                }
+                else
+                {
+                    // The touch or click lands on this note.
+                    HitNote(n, difference);
+                    break;
+                }
+            }
+
+            EmptyTouchReceiver receiver = r.gameObject.GetComponent<EmptyTouchReceiver>();
+            if (receiver != null)
+            {
+                EmptyHit(receiver.lane);
+                break;
+            }
+        }
+    }
+
+    private void HitNote(NoteObject n, float timeDifference)
+    {
+        Judgement judgement;
+        float absDifference = Mathf.Abs(timeDifference);
+        if (absDifference <= kRainbowMaxThreshold)
+        {
+            judgement = Judgement.RainbowMax;
+        }
+        else if (absDifference <= kMaxThreshold)
+        {
+            judgement = Judgement.Max;
+        }
+        else if (absDifference <= kCoolThreshold)
+        {
+            judgement = Judgement.Cool;
+        }
+        else if (absDifference <= kGoodThreshold)
+        {
+            judgement = Judgement.Good;
+        }
+        else
+        {
+            judgement = Judgement.Miss;
+        }
+
+        Debug.Log($"Hit note with {judgement}, time difference {timeDifference * 1000f} ms");
+        ResolveNote(n, judgement);
+
+        if (n.sound != "" && n.sound != UIUtils.kNone)
+        {
+            AudioClip clip = resourceLoader.GetClip(n.sound);
+            keysoundSources[n.note.lane].clip = clip;
+            keysoundSources[n.note.lane].Play();
+        }
+    }
+
+    private void EmptyHit(int lane)
+    {
+        Debug.Log("Empty hit on lane " + lane);
+    }
+
+    private void OnNoteBreak(NoteObject n)
+    {
+        Debug.Log("Break");
+        ResolveNote(n, Judgement.Break);
+    }
+
+    private void ResolveNote(NoteObject n, Judgement judgement)
+    {
+        n.gameObject.SetActive(false);
+    }
+
+    #region Pausing
     public void OnPauseButtonClickOrTouch()
     {
         stopwatch.Stop();
@@ -231,17 +424,5 @@ public class Game : MonoBehaviour
         stopwatch.Start();
         backingTrackSource.UnPause();
     }
-
-    private void OnNoteResolved(NoteObject n, Judgement judge)
-    {
-        Debug.Log(judge);
-        n.gameObject.SetActive(false);
-
-        if (n.sound != "" && n.sound != UIUtils.kNone)
-        {
-            AudioClip clip = resourceLoader.GetClip(n.sound);
-            keysoundSources[n.note.lane].clip = clip;
-            keysoundSources[n.note.lane].Play();
-        }
-    }
+    #endregion
 }
