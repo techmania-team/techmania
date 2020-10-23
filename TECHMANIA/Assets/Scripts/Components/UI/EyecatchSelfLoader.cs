a0a7ed0fd3fc74a3610c8b9539b04aedcb92f749
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-10-22 22:22:30 -0700

    Updated Unity to 2019.4.13f1. Fixed problems preventing build.

--- TECHMANIA/Assets/Scripts/Components/UI/EyecatchSelfLoader.cs
+++ TECHMANIA/Assets/Scripts/Components/UI/EyecatchSelfLoader.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class EyecatchSelfLoader : MonoBehaviour
