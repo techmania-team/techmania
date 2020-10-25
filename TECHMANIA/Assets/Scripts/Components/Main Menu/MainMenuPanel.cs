ac182f1b66b698323f8faeea3ff3234d420f5e81
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-10-24 17:25:25 -0700

    Main menu now shows game version.

--- TECHMANIA/Assets/Scripts/Components/Main Menu/MainMenuPanel.cs
+++ TECHMANIA/Assets/Scripts/Components/Main Menu/MainMenuPanel.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MainMenuPanel : MonoBehaviour
 {
+    public TextMeshProUGUI versionText;
+
+    private void Start()
+    {
+        versionText.text = Application.version;
+    }
+
     public void OnEditorButtonClick()
     {
         Curtain.DrawCurtainThenGoToScene("Editor");
