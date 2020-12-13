e0343db87ddb892acb12c82517a292b375b58a80
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-12-12 18:56:35 -0800

    Finished the skin showcase scene. VFX adjustments incoming.

--- TECHMANIA/Assets/Scripts/Components/SkinShowcase.cs
+++ TECHMANIA/Assets/Scripts/Components/SkinShowcase.cs
@@ -1,27 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkinShowcase : MonoBehaviour
 {
     public GameObject explosionMax;
     public GameObject explosionCool;
     public GameObject explosionGood;
+    public GameObject repeatExplosion;
     public GameObject holdComplete;
     public GameObject repeatHoldComplete;
     public GameObject dragComplete;
     public RectTransform explosionMaxSpawnPoint;
     public RectTransform explosionCoolSpawnPoint;
     public RectTransform explosionGoodSpawnPoint;
+    public RectTransform repeatHoldExplosionSpawnPoint;
     public RectTransform holdCompleteSpawnPoint;
     public RectTransform repeatHoldCompleteSpawnPoint;
     public RectTransform dragCompleteSpawnPoint;
+
+    public List<Image> ongoingVfx;
+    public List<GameObject> feverOverlay;
 
     private const float explosionSize = 300f;
 
     // Start is called before the first frame update
     void Start()
     {
+        ToggleOngoingVfx(true);
+        ToggleFeverOverlay(true);
     }
 
     // Update is called once per frame
@@ -34,6 +42,8 @@ public class SkinShowcase : MonoBehaviour
         SpawnOneExplosion(explosionMax, explosionMaxSpawnPoint);
         SpawnOneExplosion(explosionCool, explosionCoolSpawnPoint);
         SpawnOneExplosion(explosionGood, explosionGoodSpawnPoint);
+        SpawnOneExplosion(repeatExplosion, 
+            repeatHoldExplosionSpawnPoint);
         SpawnOneExplosion(holdComplete, holdCompleteSpawnPoint);
         SpawnOneExplosion(repeatHoldComplete, 
             repeatHoldCompleteSpawnPoint);
@@ -48,5 +58,24 @@ public class SkinShowcase : MonoBehaviour
         rect.pivot = new Vector2(0.5f, 0.5f);
         rect.sizeDelta = new Vector2(explosionSize, explosionSize);
         rect.position = spawnPoint.position;
+    }
+
+    public void ToggleOngoingVfx(bool visible)
+    {
+        foreach (Image i in ongoingVfx)
+        {
+            i.enabled = visible;
+        }
+    }
+
+    public void ToggleFeverOverlay(bool visible)
+    {
+        foreach (GameObject o in feverOverlay)
+        {
+            o.GetComponent<Image>().color = visible ?
+                Color.white :
+                Color.clear;
+            o.GetComponent<Animator>().enabled = visible;
+        }
     }
 }
