a6175301e3bb6ae0edae3d4133ddd5d4c99d9767
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2021-04-02 20:49:01 -0700

    Updated FPS report frequency to be independent of frame rate.

--- TECHMANIA/Assets/Scripts/Components/Game Scene/FpsCounter.cs
+++ TECHMANIA/Assets/Scripts/Components/Game Scene/FpsCounter.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class FpsCounter : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI text;
+    private float timeAtLastReport;
+    private const float reportInterval = 0.2f;
 
     private void Start()
     {
         text.text = "";
+        timeAtLastReport = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount % 20 == 0)
+        if (Mathf.Floor(timeAtLastReport / reportInterval) !=
+            Mathf.Floor(Time.timeSinceLevelLoad / reportInterval))
         {
             float fps = 1f / Time.smoothDeltaTime;
             text.text = fps.ToString("F2") + " FPS";
+            timeAtLastReport = Time.timeSinceLevelLoad;
         }
     }
 }
