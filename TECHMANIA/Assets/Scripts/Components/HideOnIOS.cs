6f96c2c5ef13a0f86f936ac9999915f6be0cae9f
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-06-22 19:13:06 +0800

    All graphics options are now hidden on iOS.
    https://github.com/techmania-team/techmania/commit/495eff51e30742625891306702fbebb7451914dc

---
+++ TECHMANIA/Assets/Scripts/Components/HideOnIOS.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnIOS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_IOS
+        gameObject.SetActive(false);
+#endif
+    }
+}
