7b980e912edbaa0c8f1342f274f27396745a8cff
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2022-02-22 22:10:08 -0800

    Added API to get and set text on TextElement.

--- TECHMANIA/Assets/Scripts/Theme API/VIsualElementApi.cs
+++ TECHMANIA/Assets/Scripts/Theme API/VIsualElementApi.cs
@@ -13,22 +13,48 @@ namespace ThemeApi
     [MoonSharpUserData]
     public class VisualElementApi
     {
-        private VisualElement element;
+        public VisualElement inner { get; private set; }
 
         [MoonSharpHidden]
         public VisualElementApi(VisualElement e)
         {
-            element = e;
+            inner = e;
         }
 
         #region Properties
-        public int childCount => element.childCount;
-        public bool enabledInHierarchy => element.enabledInHierarchy;
-        public bool enabledSelf => element.enabledSelf;
-        public string name => element.name;
+        public int childCount => inner.childCount;
+        public bool enabledInHierarchy => inner.enabledInHierarchy;
+        public bool enabledSelf => inner.enabledSelf;
+        public string name => inner.name;
         public VisualElementApi parent =>
-            new VisualElementApi(element.parent);
-        public bool visible => element.visible;
+            new VisualElementApi(inner.parent);
+        public bool visible => inner.visible;
+        #endregion
+
+        #region Subclass-specific properties
+        public void CheckType(System.Type type, string targetProperty)
+        {
+            if (!type.IsAssignableFrom(inner.GetType()))
+            {
+                throw new System.Exception($"VisualElement {name} is not a {type.Name}, and therefore does not have the '{targetProperty}' property.");
+            }
+        }
+
+        public bool IsTextElement() { return inner is TextElement; }
+
+        public string text
+        {
+            get
+            {
+                CheckType(typeof(TextElement), "text");
+                return (inner as TextElement).text;
+            }
+            set
+            {
+                CheckType(typeof(TextElement), "text");
+                (inner as TextElement).text = value;
+            }
+        }
         #endregion
 
         #region Query
@@ -36,14 +62,14 @@ namespace ThemeApi
         public VisualElementApi Q(string name,
             string className = null)
         {
-            return new VisualElementApi(element.Q(name, className));
+            return new VisualElementApi(inner.Q(name, className));
         }
 
         // Leave out `name` to query all elements.
         public UQueryStateApi Query(string name = null,
             string className = null)
         {
-            return new UQueryStateApi(element.Query(
+            return new UQueryStateApi(inner.Query(
                 name, className).Build());
         }
         #endregion
@@ -52,19 +78,19 @@ namespace ThemeApi
     [MoonSharpUserData]
     public class UQueryStateApi
     {
-        private UQueryState<VisualElement> state;
+        public UQueryState<VisualElement> inner { get; private set; }
         [MoonSharpHidden]
         public UQueryStateApi(UQueryState<VisualElement> s)
         {
-            state = s;
+            inner = s;
         }
 
         public void ForEach(DynValue f)
         {
             f.CheckType("UQueryStateApi.ForEach", DataType.Function);
-            state.ForEach((VisualElement e) =>
+            inner.ForEach((VisualElement e) =>
             {
-                f.Function.Call(e);
+                f.Function.Call(new VisualElementApi(e));
             });
         }
     }
