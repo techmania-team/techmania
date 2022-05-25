a61b58133a100ddb3675ded1661d5d4c050050ec
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-05-25 15:09:22 +0800

    Add pinch gesture to zoom editor workspace

--- TECHMANIA/Assets/Scripts/Components/Editor Scene/PatternPanel.cs
+++ TECHMANIA/Assets/Scripts/Components/Editor Scene/PatternPanel.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -361,6 +361,10 @@ public class PatternPanel : MonoBehaviour
                 {
                     MoveScanlineToTouch(touch.position);
                 }
+            }
+            if (Input.touchCount == 2)
+            {
+                HandleTouchResize();
             }
         }
     }
@@ -737,6 +741,21 @@ public class PatternPanel : MonoBehaviour
     #endregion
 
     #region Touch
+    private void HandleTouchResize ()
+    {
+        Touch touchZero = Input.GetTouch(0);
+        Touch touchOne = Input.GetTouch(1);
+
+        Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+        float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+        float difference = currentMagnitude - prevMagnitude;
+
+        AdjustZoom(zoom + (int) Math.Round(difference * 0.01f));
+    }
     private void MoveScanlineToTouch(Vector2 position)
     {
         Vector2 pointInHeader;
