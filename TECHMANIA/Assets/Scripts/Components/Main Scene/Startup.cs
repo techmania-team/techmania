6cde2daac8b351d2a5bcf39d4f1f78491226c181
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2023-02-25 17:47:01 -0800

    Manually merged Main Menu/Startup.cs into Main Scene/Startup.cs.

--- TECHMANIA/Assets/Scripts/Components/Main Scene/Startup.cs
+++ TECHMANIA/Assets/Scripts/Components/Main Scene/Startup.cs
@@ -40,18 +40,53 @@ public class Startup : MonoBehaviour
         Options.instance.ApplyAudioBufferSize();
         audioSourceManager.ApplyVolume();
         LoadRuleset();
-        Paths.ApplyCustomDataLocation();
-
+        
         L10n.Initialize(stringTable.text, L10n.Instance.System);
         L10n.SetLocale(Options.instance.locale, L10n.Instance.System);
 
         SpriteSheet.PrepareEmptySpriteSheet();
         Records.RefreshInstance();
-        BetterStreamingAssets.Initialize();
 
         DiscordController.Start();
         DiscordController.SetActivity(DiscordActivityType.MainMenu);
 
+#if UNITY_ANDROID
+        AndroidUtility.CheckVersion();
+        // Ask for storage permission before loading resource
+        // if custom data location is set.
+        if (Options.instance.customDataLocation)
+        {
+            StartCoroutine(AndroidUtility.AskForPermissions(
+                callback: () =>
+                {
+                    // Turn off custom data location and reset skins
+                    // if user denied permission.
+                    // Otherwise, there will be an error while loading skins.
+                    if (!AndroidUtility.HasStoragePermissions())
+                    {
+                        Options.instance.ResetCustomDataLocation();
+                    }
+                    StartBooting();
+                }));
+        }
+        else
+        {
+            // This prevents loading custom skins from streaming assets
+            // at startup. Android Play Games may sync your options after
+            // reinstall the game So we have to reset skins if custom
+            // data location is not set but using custom skins.
+            Options.instance.ResetCustomDataLocation();
+            StartBooting();
+        }
+#else
+        StartBooting();
+#endif
+    }
+
+    private void StartBooting()
+    {
+        Paths.ApplyCustomDataLocation();
+        BetterStreamingAssets.Initialize();
         bootScreen.StartBooting();
     }
 }
