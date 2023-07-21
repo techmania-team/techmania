5c1f64059edb3b36a7f320928b7be92e12baa710
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2023-07-20 21:29:07 -0700

    Added build option to override compression.

--- TECHMANIA/Assets/Scripts/Editor/BuildAssetBundleWindow.cs
+++ TECHMANIA/Assets/Scripts/Editor/BuildAssetBundleWindow.cs
@@ -3,48 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using UnityEditor.Build;
 
 public class BuildAssetBundleWindow : EditorWindow
 {
     private BuildTarget buildTarget;
+    private bool noCompression;
 
-    [MenuItem("Window/TECHMANIA/Build AssetBundles")]
+    [MenuItem("Window/TECHMANIA Theme/Build now")]
     public static void BuildForDefaultPlatform()
     {
         BuildAssetBundle(EditorUserBuildSettings.activeBuildTarget);
     }
 
-    [MenuItem("Window/TECHMANIA/Build AssetBundles (custom platform)...")]
+    [MenuItem("Window/TECHMANIA Theme/Build...")]
     private static void Init()
     {
-        BuildAssetBundleWindow window = 
+        BuildAssetBundleWindow window =
             GetWindow<BuildAssetBundleWindow>();
+        window.buildTarget = EditorUserBuildSettings.activeBuildTarget;
         window.Show();
     }
 
     private void OnGUI()
     {
-        EditorGUILayout.LabelField("Build AssetBundles for:");
+        EditorGUILayout.LabelField("Target platform:");
         buildTarget = (BuildTarget)EditorGUILayout.EnumPopup(
             buildTarget);
-        
+        noCompression = EditorGUILayout.ToggleLeft("Don't compress images (overrides import settings)", noCompression);
+
         if (GUILayout.Button("Build"))
         {
-            BuildAssetBundle(buildTarget);
+            BuildAssetBundle(buildTarget, noCompression);
         }
     }
 
-    private static void BuildAssetBundle(BuildTarget target)
+    private static void BuildAssetBundle(BuildTarget target,
+        bool noCompression = false)
     {
-        Debug.Log($"Building asset bundle for {target} ...");
+        if (noCompression)
+        {
+            EditorUserBuildSettings.overrideTextureCompression
+                = OverrideTextureCompression.ForceUncompressed;
+        }
+        Debug.Log($"Building theme for {target} ...");
         Directory.CreateDirectory(Paths.kAssetBundleFolder);
-        AssetBundleManifest manifest = 
+        AssetBundleManifest manifest =
             BuildPipeline.BuildAssetBundles(
-                Paths.kAssetBundleFolder, 
+                Paths.kAssetBundleFolder,
                 BuildAssetBundleOptions.None, target);
         foreach (string bundleName in manifest.GetAllAssetBundles())
         {
-            Debug.Log($"Built AssetBundle '{bundleName}'.");
+            Debug.Log($"Built theme at {Paths.kAssetBundleFolder}/default.");
+        }
+        if (noCompression)
+        {
+            EditorUserBuildSettings.overrideTextureCompression
+                = OverrideTextureCompression.NoOverride;
         }
     }
 }
